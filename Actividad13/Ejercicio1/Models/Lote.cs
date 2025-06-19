deee580245258da21d24c6476f48852628633231
juanmanuelczo <juanmanuelcardozo444@gmail.com>
2025-06-19 16:38:35 -0300

    iniciando ejercicio1

---
+++ Actividad13/Ejercicio1/Models/Lote.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    internal class Lote
+    {
+        int numero;
+        double[]medidas;
+        int cantidad;
+        public Lote()
+        {
+        }
+
+        public double VerMedida(int idx)
+        {
+            
+            return medidas[idx];
+        }
+        public void RegistrarMedida (double medida)
+        {
+            medidas[cantidad] = medida;
+            cantidad++;
+        }
+        public double VerCantidad (int cantidad)
+        {
+            return cantidad;
+        }
+
+        public double 
+    }
+}
